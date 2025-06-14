11a4dda8baedef086b96dd116f7be2e441bf35d8
hasnainraza00011 <xfirst166@gmail.com>
2025-06-14 22:54:08 +0500

    Delete commands.py

--- commands.py
+++
@@ -1,8 +0,0 @@
-
-from aiogram.types import Message
-
-async def help_handler(message: Message):
-    await message.answer("/voice - Convert Text to Voice\n/deposit - Deposit Money\n/balance - Check Balance\n/referral - Your Referral\n/proof - Upload Screenshot")
-
-async def learn_handler(message: Message):
-    await message.answer("This bot converts your text into voice using Novita.ai API and supports payment features.")
